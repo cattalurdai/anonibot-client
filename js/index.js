e33716c79561d065635d031a7763511d7681f769
ElCatta <cattalurdai@gmail.com>
2023-04-28 09:34:00 -0300

    fix: deleted .nf unnecessary file

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 // BOOTSTRAP FORM VALIDATIONS
 
 (function () {
-  "use strict";
-
   var forms = document.querySelectorAll(".needs-validation");
 
   Array.prototype.slice.call(forms).forEach(function (form) {
