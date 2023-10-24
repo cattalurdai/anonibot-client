04be0bd72ddc6319561946ba081267cc7745dd7c
cattalurdai <cattalurdai@gmail.com>
2023-10-24 20:47:51 -0300

    revert event

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ async function buildBody() {
   return JSON.stringify(body);
 }
 
-// const API = "https://api.anonibot.com:9999";
-  const API = "http://localhost:9999";
+ const API = "https://api.anonibot.com:9999";
+//  const API = "http://localhost:9999";
 
 function getPreviewModal() {
   if (!previewModal) {
